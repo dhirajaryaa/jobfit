3ee792780b6df11844b1804e417889a1058fd0c3
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-08-11 19:05:23 +0000

    feat(extension): react vite and crx project setup

---
+++ apps/extension/src/manifest.config.js
@@ -0,0 +1,10 @@
+import { defineManifest } from '@crxjs/vite-plugin'
+
+export default defineManifest({
+  manifest_version: 3,
+  name: 'Jobfit - Find best fit Linkedin Jobs.',
+  version: '1.0.0',
+  action: {
+    default_popup: 'index.html',
+  },
+})
